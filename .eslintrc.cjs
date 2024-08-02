d6783a19c14574b2c109674b990e6dbb587153c9
Rezwana Tasnin <30260735+rkanik@users.noreply.github.com>
2024-08-02 16:01:43 +0600

    Added prettier-plugin-sort-imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,15 @@
 module.exports = {
 	root: true,
+	ignorePatterns: ['*.cjs'],
 	parser: '@typescript-eslint/parser',
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			processor: 'svelte3/svelte3'
+		}
+	],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
@@ -16,5 +21,14 @@ module.exports = {
 		browser: true,
 		es2017: true,
 		node: true
+	},
+	rules: {
+		'@typescript-eslint/consistent-type-imports': [
+			'warn',
+			{
+				prefer: 'type-imports',
+				fixStyle: 'inline-type-imports'
+			}
+		]
 	}
-};
+}
---
+++ prettier.config.cjs
@@ -0,0 +1,40 @@
+/** @typedef  {import("prettier").Config} PrettierConfig */
+
+// @ts-expect-error SortImportsConfig missing
+/** @type { PrettierConfig | SortImportsConfig } */
+const config = {
+	semi: false,
+	useTabs: true,
+	tabWidth: 3,
+	proseWrap: 'always',
+	printWidth: 80,
+	arrowParens: 'always',
+	singleQuote: true,
+	trailingComma: 'all',
+	importOrderTypeScriptVersion: '5.0.0',
+	plugins: [
+		'prettier-plugin-svelte',
+		'@ianvs/prettier-plugin-sort-imports',
+		'prettier-plugin-tailwindcss'
+	],
+	importOrder: [
+		'<TYPES>^(node:)',
+		'<TYPES>',
+		'<TYPES>^[.]',
+		'^(react/(.*)$)|^(react$)',
+		'^(next/(.*)$)|^(next$)',
+		'<THIRD_PARTY_MODULES>',
+		'^(@ui|@base)(/.*)$',
+		'^@/(.*)$'
+	],
+	overrides: [
+		{
+			files: '*.svelte',
+			options: {
+				parser: 'svelte'
+			}
+		}
+	]
+}
+
+module.exports = config
